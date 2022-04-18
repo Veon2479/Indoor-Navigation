d359e1dbbedfc55f26a84fe20661ed9ce3e78b5f
Aliaksei Kryzhanouski <87375027+KJI0YH@users.noreply.github.com>
2022-04-18 09:45:02 +0300

    TcpListener

---
+++ Server/Server.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpServer
+{
+    class Server
+    {
+        const int DEFAULT_TCP_PORT = 4444;
+        const int DEFAULT_UDP_PORT = 4445;
+
+        static async Task Main(string[] args)
+        {
+            StartTcpListener();
+            //startUdpListener();
+        }
+
+        static async void StartTcpListener()
+        {
+            var tcpListener = new TcpListener(IPAddress.Any, DEFAULT_TCP_PORT);
+            tcpListener.Start();
+
+            try
+            {
+                await Console.Out.WriteLineAsync("Server started");
+
+                while (true)
+                {
+                    var tcpClient = await tcpListener.AcceptTcpClientAsync();
+                    await ProcessRequest(tcpClient);
+                }
+            }
+            finally
+            {
+                tcpListener.Stop();
+                await Console.Out.WriteLineAsync("Server finished");
+            }
+        }
+
+        static async Task ProcessRequest(TcpClient tcpClient)
+        {
+            //yields back to the current context when awaited
+            await Task.Yield();
+
+            //get stream of the client
+            var stream = tcpClient.GetStream();
+
+            //read data
+            using var reader = new StreamReader(stream);
+            var data = await reader.ReadLineAsync();
+            Console.WriteLine(data);
+
+            //send answer
+            using var writer = new StreamWriter(stream);
+            await writer.WriteLineAsync($"'{data}' recieved");
+            await writer.FlushAsync();
+
+            //closing connection
+            tcpClient.Close();
+        }
+
+        static async void StartUdpListener()
+        {
+            try
+            {
+                UdpClient udpClient = new UdpClient(DEFAULT_UDP_PORT);
+                IPEndPoint remotePoint = null;
+
+                byte[] bytes = udpClient.Receive(ref remotePoint);
+                string results = Encoding.UTF8.GetString(bytes);
+
+
+                udpClient.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Server/userPacket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public struct userPacket
+    {
+        public int userID;
+        public double x;
+        public double y;
+        public long time;
+
+        public userPacket(int uID, double uX, double uY, long uTime)
+        {
+            userID = uID;
+            x = uX;
+            y = uY;
+            time = uTime;
+        }
+    }
+}
