af74f6925c9b416e312177dcd5cc7ef94020151a
Aliaksei Kryzhanouski <87375027+KJI0YH@users.noreply.github.com>
2022-04-22 14:54:32 +0300

    Upgrade

---
+++ Server/gui/ServerManage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+    internal class ServerManage
+    {
+        private static string UnixToDate(long Time, string ConvertFormat)
+        {
+            DateTime ConvertedUnixTime = DateTimeOffset.FromUnixTimeSeconds(Time).DateTime;
+            return ConvertedUnixTime.ToString(ConvertFormat);
+        }
+
+        //update online users
+        public static void UpdateOnlineUsers(ref ListView view, IDModel userIDModel)
+        {
+            view.BeginUpdate();
+            view.Items.Clear();
+
+            for (int i = 1; i < userIDModel.UserTable.Count; i++)
+            {
+                if (userIDModel.UserTable[i] != IDModel.DEFAULT_TIME)
+                {
+                    ListViewItem lvItem = new ListViewItem(i.ToString());
+                    lvItem.SubItems.Add(UnixToDate(userIDModel.UserTable[i], "dd:MM:yyyy HH:mm:ss"));
+                    view.Items.Add(lvItem);
+                }
+            }
+
+            view.EndUpdate();
+        }
+
+        //start the server
+        public static void StartServer(ref Timer timer)
+        {
+
+            timer.Start();
+            Server.Start();
+        }
+
+        //stop the server
+        public static void StopServer(ref Timer timer)
+        {
+            Server.Stop();
+            timer.Stop();
+        }
+    }
+}
