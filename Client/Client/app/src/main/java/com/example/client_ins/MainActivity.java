dc19ad1f17d631943455db16e733fb41a74384b8
SunnyWorker <artem.khotin@gmail.com>
2022-04-23 00:47:43 +0300

    Design for interface

--- Client/Client/app/src/main/java/com/example/client_ins/MainActivity.java
+++ Client/Client/app/src/main/java/com/example/client_ins/MainActivity.java
@@ -16,13 +16,19 @@ public class MainActivity extends AppCompatActivity {
     TextView text1;
     TextView text2;
     TextView text3;
+    double x=0,y=0,z=0; //correct when it possible
+    double accX = 0, accY = 0, accZ = 0;
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Engine(getApplicationContext());
+        //new Engine(getApplicationContext());
+        text1 = findViewById(R.id.text1);
+        text2 = findViewById(R.id.text2);
         text3 = findViewById(R.id.text3);
+        text1.setText("Rotation\nX: "+x+"\nY: "+y+"\nZ: "+z);
+        text2.setText( "Accelerometer\nX: "+accX+"\nY: "+accY+"\nZ: "+accZ);
         text3.setText("Yes1\nYes2\nYes3\nYes4\nYes5\nYes5\nYes5\nYes5\nYes5\nYes5\nYes5\nYes5\nYes5\n");
 
     }
